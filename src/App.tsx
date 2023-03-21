399ba14e25b1a33c209f221aa5fc6300d3a203c9
verlore <johnjoseph.preston@googlemail.com>
2023-03-21 14:07:19 +0000

    First Update

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,16 @@ function App() {
 const myArrMadeFromForEach: Array<ReactElement> = []; // we create a new array which will evaluate to a value when we inject it into a JSX expression
 
 stats.forEach((item) => myArrMadeFromForEach.push(
-<div className="summary-item" key={item.category}><span><img src={item.icon} alt="" />{item.category}</span> <span>{item.score} / 100</span> </div>
+<div className={"summary-item"} id={item.category} key={item.category}><span><img src={item.icon} alt="" />{item.category}</span> <span className='summary-item-score'>{item.score} <span> / 100</span> </span> </div>
 )); 
 
 // we push a JSX element containing a value to our 'myArrMadeFromForEach' because `.forEach()` does not return any value, nor does it mutate the array on which it is called
 
+/*
 const myList = (
-  <ul>{myArrMadeFromForEach}</ul> // `myArrMadeFromForEach` will evaluate to an array of `<li>` elements
+  <div>{myArrMadeFromForEach}</div> // `myArrMadeFromForEach` will evaluate to an array of `<li>` elements
 )
-
+*/
 
   let summaryAndResults = <div className="wrapper">
   <div className="results">
@@ -24,19 +25,21 @@ const myList = (
       <p>76</p> 
       <p>of 100</p>
     </div>
+    <div className='results-bottom'>
     <h5>Great</h5>
     <p>You scored higher than 65% of the people who have taken these tests.</p>
+    </div>
   </div>
 
   <div className="summary">
-    <h3>Summary</h3>
+    <h3 className="summary-item">Summary</h3>
     
-    <div className="summary-items-container">
-    {myList}
-    </div>
+
+    {myArrMadeFromForEach}
 
 
-    <div ><button className="btn">Continue</button></div>
+
+    <div className="summary-item btn"><button >Continue</button></div>
     
   </div>
 </div>
